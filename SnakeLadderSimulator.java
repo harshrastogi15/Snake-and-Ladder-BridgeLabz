a62cb3e4464d45a7af8b372d97ed9b063052a8eb
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-23 16:32:05 +0530

    Added: Snake Ladder Game source file added

---
+++ SnakeLadderSimulator.java
@@ -0,0 +1,7 @@
+class SnakeLadderSimulator{
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder game");
+        Integer positionOfFirstPlayer = 0;
+        System.out.println(positionOfFirstPlayer);
+    }
+}
