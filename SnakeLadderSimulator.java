bce5cba1c11e3ee11562973ba5205864528bccb2
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-23 16:40:23 +0530

    Refactored: Added new method rollDice

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,7 +1,15 @@
 class SnakeLadderSimulator{
+
+    // UC2 - Roll the Dice
+    public static Integer rollDice(){
+        Integer rdm = (int)((Math.ceil(Math.random()*10))%6 + 1);
+        return rdm;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to snake ladder game");
         Integer positionOfFirstPlayer = 0;
         System.out.println(positionOfFirstPlayer);
+        System.out.println(rollDice());
     }
 }
